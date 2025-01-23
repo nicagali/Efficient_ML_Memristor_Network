805f30188910792a6f2f2756fc49f038b29d11ca
nicagali <m.conte@uu.nl>
2025-01-23 11:47:01 +0100

    Creating new branch: check_param

--- codes/check_parameters.py
+++ codes/check_parameters.py
@@ -21,24 +21,25 @@ fig.savefig(f"../plots/voltage_divider/graph.pdf")
 
 # --------- TRAIN NETWORK WITH PRESSURE ---------
 
-training_steps = 20
+training_steps = 2
 
 fig, ax = plt.subplots()
 
 G_ml = G.copy(as_view=False)
 training.train(G_ml, training_steps=training_steps, weight_type='pressure', delta_weight=1e-2, learning_rate=100)
 plotting.plot_mse(ax, fig, 'allostery_pressure') 
+ax.plot([], [], ' ', label=r'$\Delta w = 1 \times 10^{-2}$')
 
-G_ml = G.copy(as_view=False)
-training.train(G_ml, training_steps=training_steps, weight_type='pressure', delta_weight=1e-3, learning_rate=100)
-plotting.plot_mse(ax, fig, 'allostery_pressure')
+# G_ml = G.copy(as_view=False)
+# training.train(G_ml, training_steps=training_steps, weight_type='pressure', delta_weight=1e-3, learning_rate=100)
+# plotting.plot_mse(ax, fig, 'allostery_pressure_1e-3')
 
-G_ml = G.copy(as_view=False)
-training.train(G_ml, training_steps=training_steps, weight_type='pressure', delta_weight=1e-4, learning_rate=100)
-plotting.plot_mse(ax, fig, 'allostery_pressure')
+# G_ml = G.copy(as_view=False)
+# training.train(G_ml, training_steps=training_steps, weight_type='pressure', delta_weight=1e-4, learning_rate=100)
+# plotting.plot_mse(ax, fig, 'allostery_pressure_1e-4')
 
 
-
+ax.legend()
 fig.tight_layout()
 fig.savefig(f"../paper/plots/voltage_divider/mse_weights.pdf")
 
