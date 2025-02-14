f4d79be20dca714dce569ae87ee2d508f1bfa967
nicagali <m.conte@uu.nl>
2025-02-14 10:44:59 +0100

    memristance regression without bias

--- codes/training.py
+++ codes/training.py
@@ -50,7 +50,7 @@ def voltage_drop_element(circuit, result, element):
     return voltage_drop
 
 def linear_function(input):
-    return 0.7*input  + 0.5
+    return input 
 
 # --------- ALGORITHM FUNCTIONS ---------
 
