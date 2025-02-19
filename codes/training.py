486ccc1dee2555072622c3204b5844eafd162fc4
nicagali <m.conte@uu.nl>
2025-02-19 14:21:41 +0100

    Regression: Big resistor trains a input=output line -> but can not train it with bias.

--- codes/training.py
+++ codes/training.py
@@ -50,7 +50,7 @@ def voltage_drop_element(circuit, result, element):
     return voltage_drop
 
 def regression_function(input):
-    return input 
+    return 0.3*input 
 
 # --------- ALGORITHM FUNCTIONS ---------
 
